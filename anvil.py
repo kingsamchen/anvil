20bbc93e1550f6183c86f23867b80e65bf4e722a
Kingsley Chen <kingsamchen@gmail.com>
2023-11-22 01:16:49 +0800

    Add support of touch command

--- anvil.py
+++ anvil.py
@@ -3,14 +3,22 @@
 # 0xCCCCCCCC
 
 import argparse
+import os
+import pathlib
+import shutil
 
 import job_init
 
-VER = '0.9.0'
+VER = '0.10.0'
 
 
 def show_anvil_version(_):
     print('anvil %s' % VER)
+
+
+def touch_project_rules(_):
+    fp = pathlib.Path(__file__).resolve().parent / 'project_rules.toml'
+    shutil.copy(fp, os.curdir)
 
 
 def parse_args():
@@ -19,10 +27,17 @@ def parse_args():
     cmd_parser = parser.add_subparsers(title='commands', dest='command')
     cmd_parser.required = True
 
+    # touch project rules toml file.
+    touch_parser = cmd_parser.add_parser(
+        'touch', help='create project rules file')
+    touch_parser.set_defaults(func=touch_project_rules)
+
     # command init
     init_parser = cmd_parser.add_parser('init', help='bootstrap a new project')
     init_parser.set_defaults(func=job_init.run_init_job)
-    init_parser.add_argument('rule_file', help='path to the rule file to bootstrap the project', action='store')
+    init_parser.add_argument(
+        'rule_file', help='path to the rule file to bootstrap the project',
+        action='store')
 
     # command version
     ver_parser = cmd_parser.add_parser('version', help='show version and exit')
